62c720e6ac22869645a91364a7fd685e0582ea46
Student <mattray.mbr@gmail.com>
2016-02-04 12:22:30 -0700

    shits broken

---
+++ app.js
@@ -0,0 +1,108 @@
+var MainController = function($scope){
+    var num
+    $scope.lake = function(){
+        num= true
+        $scope.blue = "active"
+        $scope.red = ""
+    }
+    $scope.fire = function(){
+        
+        $scope.blue = ""
+        $scope.red = "active"
+    }
+    
+
+
+
+
+
+
+
+
+  $scope.land = [
+    'Trees',
+    'Grass',
+    'Shrubs',
+    'Trees',
+    'Grass',
+    'Shrubs',
+    'Trees',
+    'Grass',
+    'Shrubs',
+    'Trees',
+    'Grass',
+    'Shrubs',
+    'Trees',
+    'Grass',
+    'Shrubs',
+    'Trees',
+    'Grass',
+    'Shrubs',
+    'Trees',
+    'Trees',
+    'Grass',
+    'Shrubs',
+    'Trees',
+    'Grass',
+    'Shrubs',
+    'Trees',
+    'Grass',
+    'Shrubs',
+    'Trees',
+    'Grass',
+    'Shrubs',
+    'Trees',
+    'Grass',
+    'Shrubs',
+    'Trees',
+    'Grass',
+    'Shrubs',
+    'Trees',
+    'Trees',
+    'Grass',
+    'Shrubs',
+    'Trees',
+    'Grass',
+    'Shrubs',
+    'Trees',
+    'Grass',
+    'Shrubs',
+    'Trees',
+    'Grass',
+    'Shrubs',
+    'Trees',
+    'Grass',
+    'Shrubs',
+    'Trees',
+    'Grass',
+    'Shrubs',
+    'Trees',
+    'Grass',
+    'Shrubs',
+    'Trees',
+    'Grass',
+    'Shrubs',
+    'Trees',
+    'Grass',
+    'Shrubs',
+    'Trees',
+    'Grass',
+    'Shrubs',
+    'Trees',
+    'Grass',
+    'Shrubs',
+    'Trees',
+    'Grass',
+    'Shrubs',
+    'Trees',
+    'Grass',
+    'Shrubs',
+    'Trees',
+    'Grass',
+    'Shrubs',
+    'Trees'
+  ]
+}
+
+angular.module('app', [])
+  .controller('MainController', ['$scope', MainController])
